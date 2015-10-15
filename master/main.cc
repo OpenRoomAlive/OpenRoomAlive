f352d08aca16d07555ad3e107a982d4c63d9444f
Nandor Licker <licker.nandor@gmail.com>
2015-10-15 15:28:04 +0100

    [DerpVision] Parse command line options.
    Summary: Command line options parsed using boost::program_options.
    
    Test Plan:
    soliddesign
    testinprod
    
    Reviewers: michsien, piotr, hanq, ilijar
    
    Reviewed By: ilijar
    
    Maniphest Tasks: T14
    
    Differential Revision: https://ec2-52-30-7-90.eu-west-1.compute.amazonaws.com/D11

--- master/main.cc
+++ master/main.cc
@@ -5,16 +5,64 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <boost/program_options.hpp>
+
 #include "ProCam.h"
 #include "ProCamSystem.h"
 #include "MasterServer.h"
 
-int main(int argc, char **argv) {
-  (void) argc;
-  (void) argv;
+using namespace dv;
 
-  try {
+
+/**
+ * Encapsulates most of the functionality of the application.
+ */
+class MasterApplication {
+ public:
+  MasterApplication(uint16_t port)
+    : port_(port)
+  {
+    (void) port_;
+  }
+
+  int Run() {
     return EXIT_SUCCESS;
+  }
+
+ private:
+  /// Port number the server is listening on.
+  uint16_t port_;
+};
+
+
+/**
+ * Entry point of the application.
+ */
+int main(int argc, char **argv) {
+  try {
+    namespace po = boost::program_options;
+
+    // Set up the description of all command line options.
+    po::options_description description("DerpVision Master Server");
+    description.add_options()
+        ("help", "Print this message.")
+        ("port", po::value<int>()->default_value(11630), "Set the port to listen on.");
+
+    // Parse options.
+    po::variables_map options;
+    po::store(po::parse_command_line(argc, argv, description), options);
+    po::notify(options);
+
+    // Print help if requested.
+    if (options.count("help")) {
+      std::cout << description << std::endl;
+      return EXIT_SUCCESS;
+    }
+
+    // Create & run the app.
+    return MasterApplication(
+        options["port"].as<int>()
+    ).Run();
   } catch (const std::exception &ex) {
     std::cerr << "[Exception] " << ex.what() << std::endl;
     return EXIT_FAILURE;
