3b4a1e1443b3411cb6a4c7dfd09a297ef63a6320
Ilija Radosavovic <ilijar@dyn1218-188.wlan.ic.ac.uk>
2015-10-15 20:21:33 +0100

    [DerpVision][CMake] include OpenGl on mac
    Test Plan: `make`
    
    Reviewers: hanq, piotr, nand, michsien
    
    Reviewed By: nand, michsien
    
    Differential Revision: https://ec2-52-30-7-90.eu-west-1.compute.amazonaws.com/D13

--- slave/main.cc
+++ slave/main.cc
@@ -5,7 +5,11 @@
 #include <cstdlib>
 #include <string>
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 #include <GLFW/glfw3.h>
 #include <libfreenect2/libfreenect2.hpp>
 
