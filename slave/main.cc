b4bc3b2592afcae5d834fd8cff18621b31eb2bd2
Piotr <piotr.chabierski@gmail.com>
2015-10-15 19:05:50 +0100

    [DerpVision][slave] Configured CMake with a GLFW example.
    Summary: Configured project build for Linux and added toy implementation of the window creation.
    
    Test Plan: testinprod
    
    Reviewers: ilijar, michsien, hanq, nand
    
    Reviewed By: nand
    
    Differential Revision: https://ec2-52-30-7-90.eu-west-1.compute.amazonaws.com/D12

--- slave/main.cc
+++ slave/main.cc
@@ -4,11 +4,36 @@
 
 #include <cstdlib>
 #include <string>
+
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
 #include <libfreenect2/libfreenect2.hpp>
 
 int main(int argc, char **argv) {
   (void) argc;
   (void) argv;
 
+  GLFWwindow* window;
+
+  if (!glfwInit()) {
+    return EXIT_FAILURE;
+  }
+
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return EXIT_SUCCESS;
 }
+
