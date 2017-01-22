184b020395d7eb3461b69def32e60f2644eea1e2
lopho <lopho@users.noreply.github.com>
2017-01-23 00:07:45 +0100

    fix build with current versions of folly and glfw correct linker flag for glfw on linux (-lglfw) remove toStdString as toJson is already a standard string initialize folly dynamics as array instead of null

--- master/MasterApplication.cc
+++ master/MasterApplication.cc
@@ -112,7 +112,7 @@ int MasterApplication::run() {
 
     // Write the calibration data to .calibrate.
     std::ofstream(calibFile.string())
-        << folly::toJson(system_->toJSON()).toStdString()
+        << folly::toJson(system_->toJSON())
         << std::endl;
   } else {
     std::cout << "Loading calibration data..." << std::endl;
